e0402d6377c2fddc84c46b57dffdf7ebc51a6dae
Esteban <eramon@rccl.com>
2026-04-06 11:11:18 -0500

    Initial commit

---
+++ test/a1bs-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as A1BsAws from '../lib/a1bs-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/a1bs-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new A1BsAws.A1BsAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
